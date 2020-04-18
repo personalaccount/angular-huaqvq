b92f6066e2821df5f37223e6eaf2b918d73771ec
Philip Ivanov <philip.i@outlook.com>
2020-04-18 18:20:30 +0300

    added navbar logic, updated index.html

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, NavigationStart } from '@angular/router';
+import { Location, PopStateEvent } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  public isCollapsed = true;
+  private lastPoppedUrl: string;
+  private yScrollStack: number[] = [];
 
-  ngOnInit(): void {
+  constructor(public location: Location, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      this.isCollapsed = true;
+      if (event instanceof NavigationStart) {
+        if (event.url != this.lastPoppedUrl)
+          this.yScrollStack.push(window.scrollY);
+      } else if (event instanceof NavigationEnd) {
+        if (event.url == this.lastPoppedUrl) {
+          this.lastPoppedUrl = undefined;
+          window.scrollTo(0, this.yScrollStack.pop());
+        } else
+          window.scrollTo(0, 0);
+      }
+    });
+    this.location.subscribe((ev:PopStateEvent) => {
+      this.lastPoppedUrl = ev.url;
+    });
+  }
+
+  isHome() {
+    const titlee = this.location.prepareExternalUrl(this.location.path());
+
+    if ( titlee === '#/home' ) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  isDocumentation() {
+    const titlee = this.location.prepareExternalUrl(this.location.path());
+    if ( titlee === '#/documentation' ) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
 }
