60b978cc4d0c33d5b5b93c5a5eda8ca007181c7d
Philip Ivanov <philip.i@outlook.com>
2020-05-06 22:22:56 +0300

    updated routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,62 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
 
+import { CasesComponent } from './cases/cases.component';
+import { CaseDetailsComponent } from './case-details/case-details.component';
+import { CasesStatComponent } from './cases-stat/cases-stat.component';
+import { AddCasesComponent } from './add-cases/add-cases.component';
+import { EditCasesComponent } from './edit-cases/edit-cases.component';
+
 const routes: Routes = [
-  { path: '',               component: LandingComponent},
-  { path: 'user-profile',     redirectTo: '/landing' },
-  { path: 'register',         redirectTo: '/landing' },
-  { path: 'login',            component: LoginComponent }
+  {
+    path: '',
+    component: LandingComponent
+  },
+  {
+    path: 'user-profile',
+    redirectTo: '/landing'
+  },
+  {
+    path: 'register',
+    redirectTo: '/landing'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'cases',
+    component: CasesComponent,
+    data: { title: 'List of Cases' }
+  },
+  {
+    path: 'cases-details/:id',
+    component: CaseDetailsComponent,
+    data: { title: 'Cases Details' }
+  },
+  {
+    path: 'cases-stat',
+    component: CasesStatComponent,
+    data: { title: 'Cases Statistic' }
+  },
+  {
+    path: 'add-cases',
+    component: AddCasesComponent,
+    data: { title: 'Add Cases' }
+  },
+  {
+    path: 'edit-cases/:id',
+    component: EditCasesComponent,
+    data: { title: 'Edit Cases' }
+  }
 ];
 
 @NgModule({
-  imports: [ BrowserModule, RouterModule.forRoot(routes)],
-  exports: [ RouterModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
