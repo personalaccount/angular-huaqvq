0ac2355042645ec78406828e30fed4c5bc9bf34f
Philip Ivanov <philip.i@outlook.com>
2020-05-14 22:07:08 +0300

    added getCases method

--- src/app/cases-details/cases-details.component.ts
+++ src/app/cases-details/cases-details.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { Cases } from '../cases';
+
 @Component({
   selector: 'app-cases-details',
   templateUrl: './cases-details.component.html',
   styleUrls: ['./cases-details.component.css']
 })
 export class CasesDetailsComponent implements OnInit {
+  cases: Cases = {
+    _id: '',
+    name: '',
+    gender: '',
+    age: null,
+    address: '',
+    city: '',
+    country: '',
+    status: '',
+    updated: null
+  };
 
-  constructor() { }
+  isLoadingResults = true;
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private api: ApiService,
+    private router: Router
+  ) {}
+
+  getCasesDetails(id: string) {
+    this.api.getCasesById(id).subscribe((data: any) => {
+      this.cases = data;
+      console.log('Loaded cases' + this.cases);
+      this.isLoadingResults = false;
+    });
   }
 
+  ngOnInit(): void {
+    this.getCasesDetails(this.route.snapshot.params.id);
+  }
 }
