d1da067d58f028a3c95dcc552eaa1721516f2551
Philip Ivanov <philip.i@outlook.com>
2020-05-30 23:05:26 +0300

    added error display method and edit-case layout view

--- src/app/edit-cases/edit-cases.component.ts
+++ src/app/edit-cases/edit-cases.component.ts
@@ -4,6 +4,14 @@ import { ApiService } from '../api.service';
 import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 
+// Error when invalid control is dirty, touched, or submitted.
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
 @Component({
   selector: 'app-edit-cases',
   templateUrl: './edit-cases.component.html',
